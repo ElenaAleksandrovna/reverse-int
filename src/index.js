576060fddea84dbdc17e871de84b9c70dafffd13
ElenaAleksandrovna <elenasidorova9219@gmail.com>
2022-01-09 18:53:58 +0300

    init: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+let string = n + '';
+let array = string.split('');
+if (array[0] == '-'){
+    array.shift();
+}
+let reversed = array.reverse();
+let finalString = reversed.join('');
+let result = Number(finalString);
+ return result;
 }
